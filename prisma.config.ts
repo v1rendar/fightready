35a908faeaf322733fc8836c45b521e71d382462
Virendar <pavankumarvissamsetti@Vissamsettis-MacBook-Air.local>
2026-06-05 12:15:02 +0530

    remove prisma.config.ts

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'prisma/config'
-
-export default defineConfig({
-  earlyAccess: true,
-  schema: 'prisma/schema.prisma',
-  migrate: {
-    url: "postgresql://postgres.znebzbcaebmgitygysbx:Lvscb3knKVIPCX6v@aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres",
-  },
-})
